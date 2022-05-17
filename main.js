ba9b5bc07b30a4a331a1cf91143ff1f8a4452e64
Martial <Martial.torval@gmail.com>
2022-05-17 17:12:44 +0200

    adding feature : get and display random name

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
   let tabName = [];
   let namee = document.getElementById('nom-input')
   let listName = document.getElementById('liste-ajout');
+  let i = 0;
 
 
   addName = document.getElementById('ajout-liste').addEventListener('click', addNameList)
@@ -12,6 +13,26 @@
       itemName.innerText = namee.value
       console.log(tabName);
   }
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+  btnRandomList = document.getElementById('btn-aleatoire').addEventListener('click', getRandomName)
+
+  function getRandomName() {
+
+
+      for (i = 0; i < tabName.length; i++) {
+          let listRandomName = document.getElementById('liste-aleatoire');
+          let randomName = document.createElement('li');
+          listRandomName.appendChild(randomName);
+          let randomElement = Math.floor(Math.random() * tabName.length)
+          randomName.innerText = tabName[randomElement]
+      }
+  }
+
+
+
 
 
 
