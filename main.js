93c51b6535b553c6c5197e2362866ba1a09cae3b
Martial <Martial.torval@gmail.com>
2022-05-18 16:07:42 +0200

    Feature, generate random list JS and add enter keybord event

--- main.js
+++ main.js
@@ -4,15 +4,58 @@
   let i = 0;
 
 
-  addName = document.getElementById('ajout-liste').addEventListener('click', addNameList)
+  addName = document.getElementById('ajout-liste').addEventListener('click', addNameListWithClick)
+      //   namee.addEventListener('keypress', addNameList)
 
-  function addNameList() {
+  function addNameListWithClick() {
+
+      if (namee.value === tabName[i]) {
+
+          return alert('Vous avez dejà rentré ce nom !')
+      }
+
+      if (!namee.value.trim()) {
+          return alert('Veuillez rentrer un nom !')
+      }
       tabName.push(namee.value)
       let itemName = document.createElement('li');
       listName.appendChild(itemName);
-      itemName.innerText = namee.value
-      console.log(tabName);
+      itemName.innerText = namee.value;
+      //   console.log(tabName);
+
+      return namee.value = ''
   }
+
+  namee.addEventListener('keypress', addNameListWithKeyboard)
+
+  function addNameListWithKeyboard(input) {
+
+      if (input.keyCode == 13) {
+          if (namee.value === tabName[i]) {
+
+              return alert('Vous avez dejà rentré ce nom !')
+          }
+
+          if (!namee.value.trim()) {
+              return alert('Veuillez rentrer un nom !')
+          }
+          tabName.push(namee.value)
+          let itemName = document.createElement('li');
+          listName.appendChild(itemName);
+          itemName.innerText = namee.value
+          namee.value == '';
+          //   console.log(tabName);
+          //   console.log(itemName.innerText)
+          return namee.value = ''
+
+
+      }
+
+
+  }
+
+
+
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -20,21 +63,29 @@
   btnRandomList = document.getElementById('btn-aleatoire').addEventListener('click', getRandomName)
 
   function getRandomName() {
-
-
+      let listRandomName = document.getElementById('liste-aleatoire');
+      let randomTab = tabName;
+      shuffle(tabName)
       for (i = 0; i < tabName.length; i++) {
-          let listRandomName = document.getElementById('liste-aleatoire');
+
           let randomName = document.createElement('li');
           listRandomName.appendChild(randomName);
-          let randomElement = Math.floor(Math.random() * tabName.length)
-          randomName.innerText = tabName[randomElement]
+          randomName.innerText = tabName[i]
       }
+      randomTab.splice(0, tabName.length)
+
+      console.log(randomTab + "Duplicata")
+      console.log(tabName + 'TabName')
+
+
+      //   console.log(shuffle(tabName[i]))
   }
 
 
+  function shuffle(array) {
+      array.sort(() => Math.random() - 0.5)
+  }
 
-
-
-
-  //   let getRandomList = document.getElementById('btn-aleatoire')
-  //   let randomList = document.getElementById('liste-aleatoire')
+  //   let numbers = [1, 2, 3, 4, 5]
+  //   shuffle(numbers)
+  //   console.log(numbers)
---
+++ script.js
@@ -0,0 +1,5 @@
+let tab1 = [1, 2, 3, 4]
+let tab2 = [5, 6, 7, 8]
+let tab3 = tab1.concat(tab2);
+
+console.log(tab3)
