d5d44d2ec9a79f348924c35f2dae3a5dd04ad05e
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-05-18 16:03:15 +0200

    Add even/odd group list

--- main.js
+++ main.js
@@ -7,11 +7,11 @@ document.getElementById('ajout-liste').addEventListener('click', addNameList);
 
 function addNameList() {
     tabName.push(namee.value);
-    console.log(namee.value);
+    // console.log(namee.value);
     let itemName = document.createElement('li');
     listName.appendChild(itemName);
     itemName.innerText = namee.value;
-    console.log(tabName)
+    // console.log(tabName)
 
     return namee.value = "";
 }
@@ -29,10 +29,21 @@ function shuffleArray(arr) {
 function getGroup() {
     const tabBis = tabName;
     shuffleArray(tabBis);
-    console.log(tabBis.join(' '));
+    // console.log(tabBis);
+    // console.log(tabName);
 
     if (!inputNumber.value) {
         return alert('Combien de personnes par groupe veux-tu ?')
+    }
+
+    if (!tabBis.length) {
+        return alert('Les groupes sont faits')
+    }
+
+    if (tabBis.length % 2 != 0) {
+        let itemName = document.createElement('li');
+        listGroup.appendChild(itemName);
+        itemName.innerText = tabBis.splice(tabBis.length - inputNumber.value).concat(tabBis.splice(-1));
     }
 
     do {
@@ -40,5 +51,7 @@ function getGroup() {
         listGroup.appendChild(itemName);
         itemName.innerText = tabBis.splice(0, inputNumber.value).join(', ');
     } while (tabBis.length > 0);
+
+
 
 }
