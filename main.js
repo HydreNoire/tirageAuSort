1b0211700545f181f01361a4c6dafc8b4b8b92cd
Hydre <81431125+HydreNoire@users.noreply.github.com>
2022-05-19 09:57:46 +0200

    Finito

--- main.js
+++ main.js
@@ -1,27 +1,22 @@
-let tabName = [];
+let tabBase = [];
 let namee = document.getElementById('nom-input')
 let listName = document.getElementById('liste-ajout');
 let i = 0;
 
-
 addName = document.getElementById('ajout-liste').addEventListener('click', addNameListWithClick)
-//   namee.addEventListener('keypress', addNameList)
 
 function addNameListWithClick() {
-
-    if (namee.value === tabName[i]) {
-
+    if (namee.value === tabBase[i]) {
         return alert('Vous avez dejà rentré ce nom !')
     }
 
     if (!namee.value.trim()) {
         return alert('Veuillez rentrer un nom !')
     }
-    tabName.push(namee.value)
+    tabBase.push(namee.value)
     let itemName = document.createElement('li');
     listName.appendChild(itemName);
     itemName.innerText = namee.value;
-    //   console.log(tabName);
 
     return namee.value = ''
 }
@@ -29,24 +24,8 @@ function addNameListWithClick() {
 namee.addEventListener('keypress', addNameListWithKeyboard)
 
 function addNameListWithKeyboard(input) {
-
     if (input.keyCode == 13) {
-        if (namee.value === tabName[i]) {
-
-            return alert('Vous avez dejà rentré ce nom !')
-        }
-
-        if (!namee.value.trim()) {
-            return alert('Veuillez rentrer un nom !')
-        }
-        tabName.push(namee.value)
-        let itemName = document.createElement('li');
-        listName.appendChild(itemName);
-        itemName.innerText = namee.value
-        //namee.value == '';
-        //   console.log(tabName);
-        //   console.log(itemName.innerText)
-        return namee.value = ''
+        addNameListWithClick()
     }
 }
 
@@ -54,14 +33,13 @@ function shuffleArray(arr) {
     arr.sort(() => Math.random() - 0.5);
 }
 
-// ======================= Ajout aléatoire de groupes =======================//
 let inputNumber = document.getElementById('nb-groupe');
 let listGroup = document.getElementById('liste-groupe');
 
 document.getElementById('btn-groupe').addEventListener('click', getGroup);
 
 function getGroup() {
-    let tabBis = Object.values(tabName);
+    let tabBis = Object.values(tabBase);
     shuffleArray(tabBis);
 
     if (!inputNumber.value) {
@@ -83,63 +61,30 @@ function getGroup() {
         listGroup.appendChild(itemName);
         itemName.innerText = tabBis.splice(0, inputNumber.value).join(', ');
     } while (tabBis.length > 0);
-
-    // console.log(tabName);
-    // console.log(tabBis[1]);
+    document.getElementById('btn-groupe').setAttribute("disabled", "disabled");
 }
 
 inputNumber.addEventListener('keypress', getGroupkeyboard);
 
 function getGroupkeyboard(input) {
     if (input.keyCode == 13) {
-        let tabBis = Object.values(tabName);
-        shuffleArray(tabBis);
-
-        if (!inputNumber.value) {
-            return alert('Combien de personnes par groupe veux-tu ?')
-        }
-
-        if (!tabBis.length) {
-            return alert('Les groupes sont faits')
-        }
-
-        if (tabBis.length % 2 != 0) {
-            let itemName = document.createElement('li');
-            listGroup.appendChild(itemName);
-            itemName.innerText = tabBis.splice(tabBis.length - inputNumber.value).concat(tabBis.splice(-1));
-        }
-
-        do {
-            let itemName = document.createElement('li');
-            listGroup.appendChild(itemName);
-            itemName.innerText = tabBis.splice(0, inputNumber.value).join(', ');
-        } while (tabBis.length > 0);
+        getGroup();
     }
 }
 
-// ======================= Ajout liste aléatoire =======================//
 document.getElementById('btn-aleatoire').addEventListener('click', getRandomName)
 let listRandomName = document.getElementById('liste-aleatoire');
 
 function getRandomName() {
-    let randomTab = Object.values(tabName);
-    shuffleArray(tabName);
+    let randomTab = Object.values(tabBase);
+    shuffleArray(tabBase);
 
-    if (listRandomName.hasChildNodes()) {
-        alert('Tu as déjà fait ton tri')
-
-    };
-
-    for (i = 0; i < tabName.length; i++) {
+    for (i = 0; i < tabBase.length; i++) {
         let randomName = document.createElement('li');
         randomName.classList.add('nom');
         listRandomName.appendChild(randomName);
-        randomName.innerText = tabName[i];
+        randomName.innerText = tabBase[i];
     }
     randomTab.splice(0, randomTab.length)
-
-    // console.log(randomTab)
-    // console.log(tabName)
+    document.getElementById('btn-aleatoire').setAttribute("disabled", "disabled");
 }
-
-// document.querySelectorAll('nom').innerText = tabName[i]
